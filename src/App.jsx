e54d6e8282dc7afa7b34d0fc0662e85f74448e84
Paulina Siwko <pau.siwko@gmail.com>
2024-06-24 13:44:51 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+// React Bootstrap Configuration
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+function App() {
+
+  return (
+    <>
+        Kubi
+    </>
+  )
+}
+
+export default App
