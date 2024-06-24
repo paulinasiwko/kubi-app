b7656d29dce324c75d50c860e28b1c6082ddf2ec
Paulina Siwko <pau.siwko@gmail.com>
2024-06-24 15:46:04 +0200

    added homepage image

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,19 @@
 import Menu from "../components/Menu.jsx";
+import {Figure, FigureCaption, FigureImage} from "react-bootstrap";
 
-export default function Homepage() {
+
+export default function Homepage() {;
+
     return (
         <>
             <Menu />
+            <Figure>
+                <FigureImage src="../src/assets/img/background/homepage.jpg" alt="Photo of a living room" />
+                <FigureCaption>
+                    Poznaj sztukę wyrafinowanego meblarstwa
+                </FigureCaption>
+            </Figure>
+
         </>
     )
 }
