cb3d32c4c6c0e2d5a1a909da1811ae1f140e6a6e
Paulina Siwko <pau.siwko@gmail.com>
2024-06-27 19:16:00 +0200

    responsivness added to first Row of About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,9 +10,9 @@ export default function About() {
             <Row className="g-0">
                 <Image src={about1} alt="Photo of a minimalistic home office in light tones" className="aboutImg imgMain"/>
                 <div className="aboutIntro">
-                    <h5 className="fw-semibold py-4">Kim Jesteśmy?</h5>
+                    <h5 className="fw-semibold pt-md-4 pb-md-3">Kim Jesteśmy?</h5>
                     <p>
-                        Od 2020 roku firma "KUBI - Producent Mebli" dynamicznie się rozwija,
+                        Od 2020 roku firma <b className="bold">"KUBI - Producent Mebli"</b> dynamicznie się rozwija,
                         oferując najwyższej jakości usługi stolarskie w całej Polsce.
                     </p>
                 </div>
@@ -25,8 +25,8 @@ export default function About() {
                     do indywidualnych potrzeb naszych klientów.
                 </p>
                 <p>
-                    W naszej ofercie znajdują się zabudowy kuchenne, szafy, garderoby, meble pokojowe,
-                    sypialniane oraz RTV. Każdy projekt realizujemy z pasją i dbałością o detale,
+                    W naszej ofercie znajdują się <b className="bold">zabudowy kuchenne, szafy, garderoby, meble pokojowe,
+                    sypialniane oraz RTV</b>. Każdy projekt realizujemy z pasją i dbałością o detale,
                     zapewniając, że nasze meble są zarówno funkcjonalne, jak i estetyczne.
                 </p>
             </Row>
@@ -34,10 +34,9 @@ export default function About() {
                 <Col xs={12} md={6} className="px-5">
                     <h5 className="fw-semibold mb-4">Doświadczenie i Zaufanie Klientów</h5>
                     <p>
-                        Nasze doświadczenie oraz zaangażowanie pozwoliły nam zdobyć zaufanie
-                        setek zadowolonych klientów. Realizujemy zarówno małe, jak i duże projekty,
-                        dostosowując się do unikalnych potrzeb i oczekiwań. Naszą pracę wykonujemy solidnie,
-                        z najwyższą starannością, co przekłada się na trwałość i jakość naszych produktów.
+                        Nasze doświadczenie oraz zaangażowanie pozwoliły nam zdobyć zaufanie <b className="bold">setek zadowolonych klientów</b>.
+                        Realizujemy zarówno małe, jak i duże projekty, dostosowując się do unikalnych potrzeb i oczekiwań.
+                        Naszą pracę wykonujemy solidnie, z najwyższą starannością, co przekłada się na trwałość i jakość naszych produktów.
                     </p>
                 </Col>
                 <Col xs={12} md={6}>
@@ -51,7 +50,7 @@ export default function About() {
                 <Col xs={12} md={6} className="px-5">
                     <h5 className="fw-semibold mb-4">Najwyższa Jakość Materiałów i Wykonania</h5>
                     <p>
-                        Do produkcji naszych mebli używamy wyłącznie materiałów oraz akcesoriów meblowych najwyższej jakości.
+                        Do produkcji naszych mebli używamy wyłącznie materiałów oraz akcesoriów meblowych <b className="bold">najwyższej jakości</b>.
                         Wkładamy dużo pracy i serca w każdy etap realizacji, od projektu, przez dobór materiałów, aż po finalny
                         montaż. Naszym priorytetem jest zadowolenie klientów, dlatego zawsze staramy się przewyższyć ich oczekiwania,
                         oferując rozwiązania, które łączą w sobie nowoczesny design z funkcjonalnością.
@@ -64,6 +63,8 @@ export default function About() {
                     Jesteście zainteresowani? Skontaktujcie się z nami!
                     Z przyjemnością odpowiemy na wszelkie pytania, udzielimy
                     fachowych porad i rozwiejemy wszelkie wątpliwości.
+                </p>
+                <p>
                     Nasza firma istnieje, aby realizować Wasze wizje idealnych wnętrz.
                 </p>
                 <Button className="contactBtn">Kontakt</Button>
