b7a335667c9dcb28c79f5e7e473146d0133d2f38
Paulina Siwko <pau.siwko@gmail.com>
2024-06-27 09:21:47 +0200

    added footer with icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import {Col, Container, Row} from "react-bootstrap";
 import '../styles/Footer.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function Footer() {
     return (
-        <Container>
-            <Row>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
+        <Container fluid className="footer g-0">
+            <Row className="footerRow">
+                <Col><i className="bi bi-geo-alt-fill icon"></i></Col>
+                <Col><i className="bi bi-telephone-fill icon"></i></Col>
+                <Col><i className="bi bi-envelope-fill icon"></i></Col>
+                <Col><i className="bi bi-facebook icon"></i></Col>
             </Row>
         </Container>
     )
