8ac02a51e94608d910c2496fb96a1d093f73abe1
Paulina Siwko <pau.siwko@gmail.com>
2024-07-01 11:34:45 +0200

    added more info to footer + facebook link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,23 @@ export default function Footer() {
     return (
         <Container fluid className="footer g-0">
             <Row className="footerRow">
-                <Col><i className="bi bi-geo-alt-fill icon"></i></Col>
-                <Col><i className="bi bi-telephone-fill icon"></i></Col>
-                <Col><i className="bi bi-envelope-fill icon"></i></Col>
-                <Col><i className="bi bi-facebook icon"></i></Col>
+                <Col>
+                    <i className="bi bi-geo-alt-fill icon"></i>
+                    <p>ul. Jasna 6, 82-335 Jegłownik</p>
+                </Col>
+                <Col>
+                    <i className="bi bi-telephone-fill icon"></i>
+                    <p>(+48) 502-472-763</p>
+                </Col>
+                <Col>
+                    <i className="bi bi-envelope-fill icon"></i>
+                    <p>kubimeble@icloud.com</p>
+                </Col>
+                <Col>
+                    <a href="https://www.facebook.com/profile.php?id=100063485287584">
+                        <i className="bi bi-facebook icon facebook"></i>
+                    </a>
+                </Col>
             </Row>
         </Container>
     )
