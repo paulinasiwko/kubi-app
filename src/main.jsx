bd8b444f832f7d451f375b72439507cde0503b0b
Paulina Siwko <pau.siwko@gmail.com>
2024-07-02 12:05:26 +0200

    started working on contact page

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,15 @@ import Layout from "./pages/Layout.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import Gallery from "./pages/Gallery.jsx";
 import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 import SingleGallery from "./components/SingleGallery.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Layout page={<SingleGallery title="Kuchnie"
-                                   description="Zobacz, jak tworzymy kuchnie na wymiar, które doskonale łączą praktyczność z elegancją. Nasze realizacje odzwierciedlają różnorodne style i potrzeby naszych klientów. Przekonaj się, jak możemy przekształcić Twoją kuchnię w serce domu, gdzie każdy detal ma znaczenie."
-                                   folderName="kitchen" />} />
+      <Layout page={<Contact />} />
+      {/*<Layout page={<SingleGallery title="Kuchnie"*/}
+      {/*                             description="Zobacz, jak tworzymy kuchnie na wymiar, które doskonale łączą praktyczność z elegancją. Nasze realizacje odzwierciedlają różnorodne style i potrzeby naszych klientów. Przekonaj się, jak możemy przekształcić Twoją kuchnię w serce domu, gdzie każdy detal ma znaczenie."*/}
+      {/*                             folderName="kitchen" />} />*/}
   </React.StrictMode>,
 )
 
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,41 @@
+import {Col, Container, Form, Row, Button} from "react-bootstrap";
+import '../styles/Contact.css';
+
+export default function Contact() {
+    return (
+        <Container fluid className="p-0">
+            <Row className="g-0 contactBackground">
+                <Col>
+                    <Form className="form">
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Napisz do Nas!</Form.Label>
+                            <Form.Control type="email" placeholder="Email*" className="formControl" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPhone">
+                            <Form.Control type="number" placeholder="Nr telefonu (opcjonalnie)" className="formControl" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3 position-relative" controlId="formBasicText">
+                            <Form.Control as="textarea" placeholder="Treść wiadomości*" className="formControl textArea" />
+                        </Form.Group>
+
+                        <Button type="submit" className="formBtn">
+                            WYŚLIJ
+                        </Button>
+                    </Form>
+
+                </Col>
+
+                <Col>
+                    <h5 className="fw-bold">KUBI - Producent Mebli</h5>
+                    <p>Meble na wymiar Jakub Podufalski</p>
+                    <p>82-335 Jegłownik, ul. Jasna 6</p>
+                    <p>NIP: 5783140291 <br /> REGON: 385226970</p>
+                    <p>Tel: (+48) 502-472-763 <br /> E-mail: kubimeble@icloud.com</p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
