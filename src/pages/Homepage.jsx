c1c5c6b94beac1ecbb12d87c3296e90f30f564a9
Paulina Siwko <pau.siwko@gmail.com>
2024-07-04 11:02:19 +0200

    review of homepage jsx and css files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import '../styles/Homepage.css';
-import { Container, Image, Row} from "react-bootstrap";
+import { Container, Image, Row, Col } from "react-bootstrap";
 import GalleryGrid from "../components/GalleryGrid.jsx";
 
 
@@ -14,22 +14,24 @@ export default function Homepage() {
                 </div>
             </Row>
             <Row className="text-center m-5 px-lg-5">
-                <h3 className="mb-4 text-uppercase fw-semibold">Kategorie</h3>
-                <div className="m-auto">
-                    <p>
-                        Każdy z naszych projektów powstaje z myślą o Tobie – Twoich potrzebach i marzeniach.
-                        Bez względu na to, czy szukasz idealnej kuchni, czy funkcjonalnych rozwiązań do biura
-                        – u nas znajdziesz to, czego potrzebujesz.
-                    </p>
-                    <p>
-                        Przeglądaj nasze kategorie produktów i znajdź inspirację do stworzenia wymarzonego wnętrza.
-                    </p>
-                    <p>
-                        Zaufaj nam, a my pomożemy Ci w realizacji Twojej wizji, dbając o każdy detal.
-                    </p>
-                </div>
+                <Col>
+                    <h3 className="mb-4 text-uppercase fw-semibold">Kategorie</h3>
+                    <div className="m-auto">
+                        <p>
+                            Każdy z naszych projektów powstaje z myślą o Tobie – Twoich potrzebach i marzeniach.
+                            Bez względu na to, czy szukasz idealnej kuchni, czy funkcjonalnych rozwiązań do biura
+                            – u nas znajdziesz to, czego potrzebujesz.
+                        </p>
+                        <p>
+                            Przeglądaj nasze kategorie produktów i znajdź inspirację do stworzenia wymarzonego wnętrza.
+                        </p>
+                        <p>
+                            Zaufaj nam, a my pomożemy Ci w realizacji Twojej wizji, dbając o każdy detal.
+                        </p>
+                    </div>
+                </Col>
             </Row>
-            <GalleryGrid />
+            <GalleryGrid/>
         </Container>
     )
 }
